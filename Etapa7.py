27f32ad6222638e3e0c95e7e0630998edfea344c
Milord10 <miguel.diniz@live.com.pt>
2018-12-17 11:51:39 +0000

    Update Etapa7.py

--- Etapa7.py
+++ Etapa7.py
@@ -1,12 +1,12 @@
 def simulador(nome_ficheiro):
-    InternalFile=open('nome_ficheiro','w')
+    InternalFile=open('nome_ficheiro','r')
+    import InternalFile  #Essta Merda tátoda fodida
     if type(Months)==int:
-        EscapeGoat2=2 #Apaga isto é só um bode expiatório
-
+        simular_populacoes (Months,Eco)
     elif Months=="q" or Months=="Q":
         FinalAnswer="Simulador Terminado."
     elif type(Months)!=int and not Months=="q" and not Months=="Q":
         FinalAnswer="Caracter inválido!"
 
     return FinalAnswer
-print(simulador('Ecosystem.txt'))
+print(simulador('configuracao.txt'))
