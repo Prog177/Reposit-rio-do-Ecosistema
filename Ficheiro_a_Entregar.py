c24fc71a1aaaeda8d674ceeec9fcbb7f916940a2
Milord10 <miguel.diniz@live.com.pt>
2018-12-18 12:25:50 +0000

    Update Ficheiro_a_Entregar.py

--- Ficheiro_a_Entregar.py
+++ Ficheiro_a_Entregar.py
@@ -5,7 +5,7 @@
 
 #Pce0=50000
 #Rce=1
-#Pcemax=100000
+#Pcemax=10000
 #Mce = 5
 #Pco0 = 50
 #Pcomax = 1000
@@ -15,22 +15,37 @@
 #Mlo = 0.0005
 q="q"
 Q="Q"
-Eco={0:50000,1:50,2:5}
+Eco={0: 50000, 1: 1, 2: 100000, 3: 5, 4:50,5:1000,6: 2,7:5,8:100,9:0.0005}
 def evoluir_populacao_cenouras (Pce,Rce,Pcemax,Mce,Pco):
+    Pce=int(Pce)
+    Rce=int(Rce)
+    Pcemax=int(Pcemax)
+    Mce=int(Mce)
+    Pco=int(Pco)
     PceAfter=Pce+((Rce*Pce)*(1-(Pce/Pcemax)))-(Mce*Pco)
     return round(PceAfter)
 #Aqui começa a Etapa 2
 
 def evoluir_populacao_coelhos (Pco,Pce,Mce,Pcomax,Mco,Plo):
+    Pco=int(Pco)
+    Pce=int(Pce)
+    Mce=int(Mce)
+    Pcomax=int(Pcomax)
+    Mco=int(Mco)
+    Plo=int(Plo)
     Nco=(Pce/(Mce*Pco))
     PcoAfter=Pco+Nco*(1-(Pco/Pcomax))-(Mco*Plo)
     return round(PcoAfter)
 #Aqui começa a Etapa 3
 def evoluir_populacao_lobos (Plo,Pco,Mco,Plomax,Mlo):
+    Mlo=float(Mlo)
+    Plo=int(Plo)
+    Pco=int(Pco)
+    Mco=int(Mco)
+    Plomax=int(Plomax)
     Nlo=(Pco/(Mco*Plo))
     PloAfter=Plo+Nlo*(1-(Plo/Plomax))-(Mlo*Plo)
     return round(PloAfter)
-#Aqui começa a Etapa 4
 #Aqui começa a Etapa 4
 def simular_populacoes (num_meses,param):
     Iteration=0
@@ -40,20 +55,17 @@ def simular_populacoes (num_meses,param):
     FinalReturn=""
     if Iteration==0:
         EcosystemPython.write(('Mês '+str(Iteration)+' : '+str(50000) +' cenouras,'+str(50)+' coelhos,'+str(5)+' lobos')+('\n'))
-        Iteration+=3
+        Iteration+=10
         Month+=1
-    while Iteration<=(num_meses*3) and not Iteration==0:
-        CenourasIntermidiate=evoluir_populacao_cenouras (Pce0,Rce,Pcemax,Mce,Pco0)
-        CoelhosIntermidiate=evoluir_populacao_coelhos (Pco0,Pce0,Mce,Pcomax,Mco,Plo0)
-        LobosIntermidiate=evoluir_populacao_lobos (Plo0,Pco0,Mco,Plomax,Mlo)
-        Pce0=CenourasIntermidiate
-        Pco0=CoelhosIntermidiate
-        Plo0=LobosIntermidiate
+    for Iteration in range(0,num_meses) and not Iteration==0:
+        CenourasIntermidiate=evoluir_populacao_cenouras (param[Iteration+0],param[1],param[2],param[3],param[Iteration+4])
+        CoelhosIntermidiate=evoluir_populacao_coelhos (param[Iteration+4],param[Iteration+0],param[3],param[5],param[6],param[Iteration+7])
+        LobosIntermidiate=evoluir_populacao_lobos (param[Iteration+7],param[Iteration+4],param[6],param[8],param[9])
         param[Iteration] = str(CenourasIntermidiate)
-        param[Iteration+1] = str(CoelhosIntermidiate)
-        param[Iteration+2] = str(LobosIntermidiate)
+        param[Iteration+4] = str(CoelhosIntermidiate)
+        param[Iteration+7] = str(LobosIntermidiate)
         EcosystemPython.write(('Mês '+str(Month)+' : '+str(Pce0) +' cenouras,'+str(Pco0)+' coelhos,'+str(Plo0)+' lobos')+('\n'))
-        Iteration+=3
+        Iteration+=10
         Month+=1
     for Iteration2 in range(0,Iteration,3):
         FinalReturn+=('Mês '+str(MonthFor)+' : '+str(param[Iteration2])+' cenouras,'+str(param[Iteration2+1])+' coelhos,'+str(param[Iteration2+2])+' lobos')+('\n')
@@ -70,28 +82,49 @@ def menu():
         print("Caracter inválido!")
         menu()
     elif type(Months)==int:
-        Output=menu()
-    elif  Months=="q" or Months=="Q":
         Output=Months
+    elif  Months=="q" or Months=="Q":
+        Output="Simulador Terminado."
     return Output
     
 #Aqui começa a Etapa 6
+def obter_parametros(nome_ficheiro):
+    parametros = {}
+    nome = []
+    valor = []
+    for linha in nome_ficheiro:
+        n,v = linha.split('=')
+        nome.append(n)
+        valor.append(eval(v))
+    for i in range(0, len(nome)):
+        parametros.update({nome[i]:valor[i]})
+    return parametros
 #Aqui começa a Etapa 7
 def simulador(nome_ficheiro):
-    DicEco = {}
-    InternalFile=open(nome_ficheiro,'r')
-    for Lines in InternalFile:
-        palavras = Lines.split("=")        #Altera esta merda toda 
-        DicEco[palavras[0]] = float(palavras[1])
+    
 
+    #DicEco=obter_parametros(nome_ficheiro)
     InternalFile.close()
     print(DicEco)
 
- 
-    FinalAnswer=simular_populacoes(menu(),Eco)
+    FuntionX=menu()
+    FinalAnswer=simular_populacoes(FuntionX,DicEco)
     return FinalAnswer
 print(simulador('configuracao.txt'))
 
 #Aqui começa a Etapa 8
 #Aqui começa a Etapa 9
+def gravar_resultado_simulacao(nome_ficheiro, lista_pop_cenouras, lista_pop_coelhos, lista_pop_lobos):
+    resultados = open('resultados.csv', 'a+')
+    resultados.write('Mês' + ';')
+    resultados.write('Cenouras' + ';')
+    resultados.write('Coelhos' + ';')
+    resultados.write('Lobos' + '\n')
+    for i in range(0, len(lista_pop_cenouras)):
+        resultados.write(str(i) + ';')
+        resultados.write(str(lista_pop_cenouras[i]) + ';')
+        resultados.write(str(lista_pop_coelhos[i]) + ';')
+        resultados.write(str(lista_pop_lobos[i]) + '\n')
+    return resultados
+
 
